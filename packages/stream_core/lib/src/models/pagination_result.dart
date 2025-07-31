4575f8227ada6baf7dbecf1e54b78479657619bc
Rene Floor <rene.floor@getstream.io>
2025-07-31 13:29:16 +0200

    Add pagination models

---
+++ packages/stream_core/lib/src/models/pagination_result.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class PaginationResult<T> {
+  const PaginationResult({
+    required this.items,
+    required this.pagination,
+  });
+
+  final List<T> items;
+  final PaginationData pagination;
+}
+
+class PaginationData extends Equatable {
+  const PaginationData({
+    this.next,
+    this.previous,
+  });
+
+  /// Item id of where to start searching from for next [results]
+  final String? next;
+
+  /// Item id of where to start searching from for previous [results]
+  final String? previous;
+
+  @override
+  List<Object?> get props => [next, previous];
+}
