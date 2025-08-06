3b033f27ddf7adbba65bb62cb006a3ee8f483d0e
Rene Floor <rene.floor@getstream.io>
2025-08-06 15:37:36 +0200

    chore(llc): downgrade collection for flutter 3.27 (#5)
    * downgrade collection for flutter 3.27
    
    * export some more classes

--- packages/stream_core/lib/src/ws.dart
+++ packages/stream_core/lib/src/ws.dart
@@ -1,5 +1,12 @@
 export 'ws/client/connection_recovery_handler.dart';
 export 'ws/client/default_connection_recovery_handler.dart';
-export 'ws/client/web_socket_client.dart' show CloseCode, WebSocketClient;
+export 'ws/client/web_socket_client.dart'
+    show
+        CloseCode,
+        EventDecoder,
+        PingReguestBuilder,
+        VoidCallback,
+        WebSocketClient;
 export 'ws/client/web_socket_connection_state.dart';
+export 'ws/events/sendable_event.dart';
 export 'ws/events/ws_event.dart';
