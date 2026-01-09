e49d7ca5662bc9dd23f12a65c9c6747d6a8601da
Rene Floor <rene.floor@getstream.io>
2026-01-09 13:48:44 +0100

    poc of button and gallery

---
+++ packages/stream_core_flutter/lib/src/theme/stream_component_factory.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:stream_core_flutter/stream_core_flutter.dart';
+
+import '../components/stream_button.dart' show DefaultStreamButton;
+
+typedef StreamComponentBuilder<T> =
+    Widget Function(BuildContext context, T props);
+
+class StreamComponentFactory {
+  StreamComponentFactory({
+    StreamComponentBuilder<StreamButtonProps>? buttonFactory,
+  }) : buttonFactory = buttonFactory ?? DefaultStreamButton.factory;
+
+  StreamComponentBuilder<StreamButtonProps> buttonFactory;
+}
---
+++ packages/stream_core_flutter/lib/src/theme/stream_theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:stream_core_flutter/src/theme/stream_component_factory.dart';
+import 'package:stream_core_flutter/stream_core_flutter.dart';
+
+class StreamTheme extends ThemeExtension<StreamTheme> {
+  StreamTheme({
+    StreamComponentFactory? componentFactory,
+    this.primaryColor,
+    StreamButtonTheme? buttonTheme,
+  }) : componentFactory = componentFactory ?? StreamComponentFactory(),
+       buttonTheme = buttonTheme ?? StreamButtonTheme();
+
+  final StreamComponentFactory componentFactory;
+
+  final Color? primaryColor;
+
+  final StreamButtonTheme buttonTheme;
+
+  static StreamTheme of(BuildContext context) {
+    return Theme.of(context).extension<StreamTheme>() ?? StreamTheme();
+  }
+
+  @override
+  StreamTheme copyWith({
+    Color? primaryColor,
+    StreamButtonTheme? buttonTheme,
+  }) {
+    return StreamTheme(
+      componentFactory: componentFactory,
+      primaryColor: primaryColor ?? this.primaryColor,
+      buttonTheme: buttonTheme ?? this.buttonTheme,
+    );
+  }
+
+  @override
+  ThemeExtension<StreamTheme> lerp(
+    covariant ThemeExtension<StreamTheme>? other,
+    double t,
+  ) {
+    if (other is! StreamTheme) {
+      return this;
+    }
+    return StreamTheme();
+  }
+}
