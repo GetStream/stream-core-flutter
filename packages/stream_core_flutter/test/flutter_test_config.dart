8ee70bbc637a0ce494c8932665685ab4d4aecbf6
Rene Floor <rene.floor@getstream.io>
2026-01-29 14:15:06 +0100

    disable CI snapshots for local tests

--- packages/stream_core_flutter/test/flutter_test_config.dart
+++ packages/stream_core_flutter/test/flutter_test_config.dart
@@ -1,17 +1,18 @@
-// ignore_for_file: do_not_use_environment
-
 import 'dart:async';
+import 'dart:io';
 
 import 'package:alchemist/alchemist.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  final isCI = const String.fromEnvironment('IS_CI', defaultValue: 'false').toLowerCase() == 'true';
+  final isRunningInCi = Platform.environment.containsKey('GITHUB_ACTIONS');
 
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
+      // Enable golden tests for CI environments and disable them for local environments.
+      ciGoldensConfig: CiGoldensConfig(enabled: isRunningInCi),
       // Disable platform-specific goldens to ensure consistent results across
       // different machines and CI environments.
-      platformGoldensConfig: PlatformGoldensConfig(enabled: !isCI),
+      platformGoldensConfig: PlatformGoldensConfig(enabled: !isRunningInCi),
     ),
     run: testMain,
   );
