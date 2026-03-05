be2ddf96a03a28cc43994112cdc11a354b0dfd78
Sahil Kumar <sahil@getstream.io>
2026-03-06 04:48:27 +0530

    feat(ui): add StreamFlex component with playground and showcase use cases

--- apps/design_system_gallery/lib/app/gallery_app.directories.g.dart
+++ apps/design_system_gallery/lib/app/gallery_app.directories.g.dart
@@ -32,6 +32,8 @@ import 'package:design_system_gallery/components/buttons/stream_emoji_button.dar
     as _design_system_gallery_components_buttons_stream_emoji_button;
 import 'package:design_system_gallery/components/common/stream_checkbox.dart'
     as _design_system_gallery_components_common_stream_checkbox;
+import 'package:design_system_gallery/components/common/stream_flex.dart'
+    as _design_system_gallery_components_common_stream_flex;
 import 'package:design_system_gallery/components/common/stream_progress_bar.dart'
     as _design_system_gallery_components_common_stream_progress_bar;
 import 'package:design_system_gallery/components/context_menu/stream_context_menu.dart'
@@ -378,6 +380,21 @@ final directories = <_widgetbook.WidgetbookNode>[
                 builder:
                     _design_system_gallery_components_common_stream_checkbox
                         .buildStreamCheckboxShowcase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'StreamFlex',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Playground',
+                builder: _design_system_gallery_components_common_stream_flex
+                    .buildStreamFlexPlayground,
+              ),
+              _widgetbook.WidgetbookUseCase(
+                name: 'Showcase',
+                builder: _design_system_gallery_components_common_stream_flex
+                    .buildStreamFlexShowcase,
               ),
             ],
           ),
