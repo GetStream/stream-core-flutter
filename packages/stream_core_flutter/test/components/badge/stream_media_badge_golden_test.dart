5becf870e201bfa9269c22ec8a4d143172ee5431
Rene Floor <rene.floor@getstream.io>
2026-03-19 16:52:15 +0100

    ignore redundant arguments rule

--- packages/stream_core_flutter/test/components/badge/stream_media_badge_golden_test.dart
+++ packages/stream_core_flutter/test/components/badge/stream_media_badge_golden_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_redundant_argument_values
+
 import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
